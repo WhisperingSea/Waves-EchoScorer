e863a52dbd419c85c1853160605083e1aa169adb
Sohansc13 <165148046+Sohansc13@users.noreply.github.com>
2024-09-29 14:39:50 +0530

    HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Characters from "./pages/Characters.tsx";
 import "./styles/App.css";
